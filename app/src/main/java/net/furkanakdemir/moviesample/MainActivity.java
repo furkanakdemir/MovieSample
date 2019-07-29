46a51be1ed9b3cabbf7b6b12b4b72e249c11a67c
Furkan Akdemir <frukan@gmail.com>
2019-07-29 22:57:24 +0300

    Add a search widget in Movie list

--- app/src/main/java/net/furkanakdemir/moviesample/MainActivity.java
+++ app/src/main/java/net/furkanakdemir/moviesample/MainActivity.java
@@ -1,8 +1,6 @@
 package net.furkanakdemir.moviesample;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
@@ -15,25 +13,5 @@ public class MainActivity extends DaggerAppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/net/furkanakdemir/moviesample/ui/MovieListFragment.java
+++ app/src/main/java/net/furkanakdemir/moviesample/ui/MovieListFragment.java
@@ -1,11 +1,17 @@
 package net.furkanakdemir.moviesample.ui;
 
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -17,12 +23,15 @@ import net.furkanakdemir.moviesample.data.Movie;
 import net.furkanakdemir.moviesample.data.MovieRepository;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.PublishSubject;
 import timber.log.Timber;
 
 
@@ -31,12 +40,20 @@ import timber.log.Timber;
  */
 public class MovieListFragment extends BaseFragment implements MovieAdapter.OnMovieCallback {
 
+    private static final long DELAY_SEARCH_IN_MS = 800;
+
+
     @BindView(R.id.moviesRecyclerView)
     RecyclerView moviesRecyclerView;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     private MovieAdapter movieAdapter;
 
     private MovieRepository movieRepository;
+
+    PublishSubject<String> publishSubject = PublishSubject.create();
 
 
     public MovieListFragment() {
@@ -49,10 +66,72 @@ public class MovieListFragment extends BaseFragment implements MovieAdapter.OnMo
         return R.layout.fragment_movie_list;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_movie_list, menu);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        setupSearchView(searchView);
+    }
+
+    @SuppressLint("CheckResult")
+    private void setupSearchView(@NonNull SearchView searchView) {
+
+        searchView.setIconifiedByDefault(false);
+        searchView.setIconified(false);
+
+        publishSubject
+                .debounce(DELAY_SEARCH_IN_MS, TimeUnit.MILLISECONDS)
+                .distinctUntilChanged()
+                .skip(1)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableObserver<String>() {
+                    @Override
+                    public void onNext(String query) {
+                        Timber.i("Query %s", query);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                publishSubject.onNext(query);
+                return false;
+
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                publishSubject.onNext(newText);
+
+                return false;
+            }
+        });
+    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setupToolbar();
+
 
         movieRepository = new FakeMovieRepository();
 
@@ -84,6 +163,15 @@ public class MovieListFragment extends BaseFragment implements MovieAdapter.OnMo
 
                     }
                 });
+    }
+
+    private void setupToolbar() {
+        toolbar.setNavigationIcon(null);
+
+        ((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) requireActivity()).getSupportActionBar().setDisplayShowHomeEnabled(false);
+        ((AppCompatActivity) requireActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
+        ((AppCompatActivity) requireActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(false);
     }
 
     @Override
