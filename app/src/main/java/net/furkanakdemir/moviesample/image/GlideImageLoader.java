60f0232b1e601cddac5a8f3cb1a6a80d9be1edc6
Furkan Akdemir <frukan@gmail.com>
2019-08-03 11:17:26 +0300

    Add placeholder for images

--- app/src/main/java/net/furkanakdemir/moviesample/image/GlideImageLoader.java
+++ app/src/main/java/net/furkanakdemir/moviesample/image/GlideImageLoader.java
@@ -5,6 +5,8 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
+import net.furkanakdemir.moviesample.R;
+
 import javax.inject.Inject;
 
 public class GlideImageLoader implements ImageLoader {
@@ -20,6 +22,10 @@ public class GlideImageLoader implements ImageLoader {
 
     @Override
     public void load(ImageView imageView, String imageUrl) {
-        Glide.with(context).load(imageUrl).into(imageView);
+        Glide.with(context)
+                .load(imageUrl)
+                .placeholder(R.drawable.ic_movie)
+                .error(R.drawable.ic_movie)
+                .into(imageView);
     }
 }
